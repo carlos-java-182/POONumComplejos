f7d343cc1fa90a33ca5bab811e23ed0f6432ca11
carlos-java-182 <carlos.villasenor@outlook.com>
2021-05-31 22:46:31 -0500

    Java

---
+++ src/ejercicio/NumeroComplejo.java
@@ -0,0 +1,40 @@
+package ejercicio;
+
+public class NumeroComplejo {
+	
+	//Atributos
+	
+	private double a;
+	private double b;
+	
+	
+	
+	//Métodos
+	
+	public NumeroComplejo(double a, double b) {
+		
+		this.a = a;
+		this.b = b;
+	}
+
+
+
+	public double getA() {
+		return a;
+	}
+
+
+
+	public double getB() {
+		return b;
+	}
+
+
+
+	
+	
+	
+	
+	
+
+}
